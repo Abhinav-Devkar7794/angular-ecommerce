ff42a0a25ef5c48d4cf777e8cdf538b8d6d54b8f
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2020-07-03 04:32:36 +0100

    add images and create home page with product wise filter

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,29 +4,51 @@ import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
+  
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
   constructor(private httpClient : HttpClient) { }
 
-  getProductList() : Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId : number) : Observable<Product[]>{
+    const serchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponseProducts>(serchUrl).pipe(
+
+      //@ToDo : need to build URL based on category id ... will come back to this !
 
       map(response => response._embedded.products)
     );
   }
 
+  getProductcategories() : Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+
+      //@ToDo : need to build URL based on category id ... will come back to this !
+
+      map(response => response._embedded.productsCategory)
+  }
+
 
 }
-interface GetResponse{
+interface GetResponseProducts{
 
   _embedded : {
 
     products : Product[];
+  }
+    
+}
+
+interface GetResponseProductCategory{
+
+  _embedded : {
+
+    productsCategory : ProductCategory[];
   }
     
 }
